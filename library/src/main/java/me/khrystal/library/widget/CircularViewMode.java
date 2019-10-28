b1bbe78288ed80efe0cce017dd0e1a1a6f87e551
jbs-fm <36034436+jbs-fm@users.noreply.github.com>
2019-10-28 11:13:53 +0000

    Migrate to androidx

--- library/src/main/java/me/khrystal/library/widget/CircularViewMode.java
+++ library/src/main/java/me/khrystal/library/widget/CircularViewMode.java
@@ -1,8 +1,8 @@
 package me.khrystal.library.widget;
 
-import android.support.v4.view.ViewCompat;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import androidx.core.view.ViewCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.View;
 
 /**
